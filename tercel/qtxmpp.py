85da9df35950b41c05a28e17c1d8f90e8d9aa64a
Jerome Leclanche <adys@azura>
2011-06-15 17:45:42 +0100

    Add parent() to QXMPPClient, add dev debug logging

--- tercel/qtxmpp.py
+++ tercel/qtxmpp.py
@@ -6,4 +6,13 @@ Qt wrapper around SleekXMPP
 from sleekxmpp import ClientXMPP
 
 class QXMPPClient(ClientXMPP):
-	pass
+	def __init__(self, parent=None, *args):
+		super(ClientXMPP, self).__init__(*args)
+		self._parent = parent
+	
+	def parent(self):
+		return self._parent
+
+# Remove for production
+import logging
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
