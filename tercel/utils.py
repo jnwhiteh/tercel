6844181ea6a046500fd9624dc85ea7542ae54551
Jerome Leclanche <adys@azura>
2011-06-18 11:01:28 +0100

    Oops

--- tercel/utils.py
+++ tercel/utils.py
@@ -6,9 +6,11 @@ Util functions
 from time import strftime
 
 
-def messageToDict(message)
+def messageToDict(message):
+	message = dict(message)
 	message["from_resource"] = message["from"].resource
 	message["from"] = message["from"].bare
 	message["to_resource"] = message["to"].resource
 	message["to"] = message["to"].bare
 	message["timestamp"] = strftime("[%H:%M:%S]")
+	return message
