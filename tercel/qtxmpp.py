613626831c07e305ef16e029878c76cffccbf995
Jerome Leclanche <adys@azura>
2011-06-18 10:51:20 +0100

    Whitespace change

--- tercel/qtxmpp.py
+++ tercel/qtxmpp.py
@@ -5,6 +5,7 @@ Qt wrapper around SleekXMPP
 
 from sleekxmpp import ClientXMPP
 
+
 class QXMPPClient(ClientXMPP):
 	def __init__(self, parent=None, *args):
 		super(QXMPPClient, self).__init__(*args)
