88482402a0a03b493c5a48d94f2a044aa49e5e38
Jerome Leclanche <adys@azura>
2011-06-18 10:50:33 +0100

    Add utils.messageToDict()

---
+++ tercel/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Util functions
+"""
+
+from time import strftime
+
+
+def messageToDict(message)
+	message["from_resource"] = message["from"].resource
+	message["from"] = message["from"].bare
+	message["to_resource"] = message["to"].resource
+	message["to"] = message["to"].bare
+	message["timestamp"] = strftime("[%H:%M:%S]")
